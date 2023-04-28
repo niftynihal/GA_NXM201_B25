0a91f9ae82d16b75323292790fc7f5e06215e397
Nihal <niftynihal@gmail.com>
2023-04-28 12:47:06 +0530

    GA class code

---
+++ index.js
@@ -0,0 +1,14 @@
+const str = "hello"
+
+console.log(str)
+
+const str2 = "world"
+
+if (str.length > str2.length) {
+    console.log("trueeeeeee")
+} else {
+    console.log("false....")
+}
+
+
+// 12 : 40pm
